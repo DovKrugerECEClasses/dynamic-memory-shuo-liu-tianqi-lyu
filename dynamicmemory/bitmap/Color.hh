d24577bdd22df4322d63bae4053fae2470c328d3
leo81601 <36606875+leo81601@users.noreply.github.com>
2018-11-08 02:02:54 -0500

    Second edit of the Color.hh and some methods of bitmap

--- dynamicmemory/bitmap/Color.hh
+++ dynamicmemory/bitmap/Color.hh
@@ -7,6 +7,18 @@ public:
 	Color(uint8_t r, uint8_t g, uint8_t b) {
         rgba = (r << 16) + (g << 8) + b;
 	}
+
+	char getColor(){
+	    if(rgba == 0xFF0000){
+            return 'R';
+	    } else if(rgba == 0x0000FF){
+            return 'B';
+	    } else if(rgba == 0x00FF00){
+            return 'G';
+	    } else if(rgba == 0xFFFFFF){
+            return 'W';
+	    }
+	}
 	//TODO:	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha) {}
 	
 };
--- dynamicmemory/bitmap/main.cc
+++ dynamicmemory/bitmap/main.cc
@@ -7,10 +7,10 @@ private:
     uint32_t* rgba;
     uint32_t width, height;
 public:
-    Bitmap(unit32_t width, unit32_t height, uint32_t color = 0x000000):rgba(new uint32_t[width * height]), width(width), height(height){
+    Bitmap(unit32_t width, unit32_t height, uint32_t defa = 0x000000):rgba(new char[width * height]), width(width), height(height){
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                rgba[i * height + j] = color;
+                rgba[i * height + j] = defa;
             }
         }
     }
@@ -18,34 +18,71 @@ public:
         delete [] rgba;
     }
 
-    void line(uint32_t x1, uint32_t x2, uint32_t y1, uint32_t y2, Color c){
-        for (int i = x1; i <= x2 ; i++) {
-            for (int j = y1; j <= j2 ; j++) {
-                rgba[j * height + i] = c;
+    void line(uint32_t x1, uint32_t x2, uint32_t y1, uint32_t y2, Color& c){
+        double deltax = x2 - x1;
+        double deltay = y2 - y1;
+        double deltaerr = 0;
+        if(deltay / deltax < 0){
+            deltaerr = -(deltay / deltax);
+        } else{
+            deltaerr = deltay / deltax;
+        }
+        double error = 0;
+        int a = y1;
+        for (int i = x1; i <= x2; i++){
+            rgba[i * height + a] = c.getColor();
+            error = error + deltaerr;
+            if(error >= 0.5){
+                if(deltay > 0){
+                    a = a + 1;
+                } else if(deltay = 0){
+                    a = a;
+                } else{
+                    a = a - 1;
+                }
+                error = error - 1;
             }
         }
     }
 
-    void horizLine(uint32_t x1, uint32_t x2, uint32_t y, Color c){
+    void horizLine(uint32_t x1, uint32_t x2, uint32_t y, Color& c){
         for (int i = x1; i <= x2; i++) {
-            rgba[y * height + i] = uint32_t val;
+            rgba[y * height + i] = c.getColor();
         }
     }
 
-    void vertLine(uint32_t x, uint32_t y1, uint32_t y2, Color c){
+    void vertLine(uint32_t x, uint32_t y1, uint32_t y2, Color& c){
         for (int i = y1; i <= y2 ; i++) {
-            rgba[i * height + x] = c;
+            rgba[i * height + x] = c.getColor();
         }
     }
 
-    void vertLine(uint32_t x, uint32_t y1, uint32_t y2, Color c){
-        for (int i = y1; i <= y2 ; i++) {
-            rgba[i * height + x] = c;
+    void fillRect(uint32_t x1, uint32_t y1, uint32_t Width, uint32_t Height, Color& c){
+        for (int i = x1; i <= x1 + Width; i++) {
+            for (int j = y1; j <= y1 + Height; j++){
+                rgba[j * height + i] = c.getColor();
+            }
         }
     }
 
-    Bitmap& ellipse(uint32_t xCenter, uint32_t yCenter, uint32_t width, uint32_t height, Color c){
+    void drawRect(uint32_t x1, uint32_t y1, uint32_t Width, uint32_t Height, Color& c){
+        for (int i = x1; i <= x1 + Width; i++) {
+            for (int j = y1; j <= y1 + Height; j++){
+                if (j == y1 || j == y1 + Height - 1){
+                    rgba[j * height + i] = c.getColor();
+                } else{
+                    if(i == x1 || i == x1 + Width - 1){
+                        rgba[j * height + i] = c.getColor();
+                    }
+                }
+            }
+        }
+    }
 
+    void ellipse(uint32_t xCenter, uint32_t yCenter, uint32_t Width, uint32_t Height, Color& c){
+        for(int i = xCenter - (Width / 4) + 1; i <= xCenter + (Width / 4); i++){
+            rgba[i]
+        }
     }
 };
 
@@ -54,8 +91,8 @@ int main() {
 	Bitmap b(30,20); // 30 pixels across, 20 pixels high, each pixel RGBA
 	// top left pixel = (0,0)
 	Color RED(255,0,0); // all red, no green, no blue (fully opaque)
-	Color BLUE(0,255,0);
-	Color GREEN(0,0,255);
+	Color GREEN(0,255,0);
+	Color BLUE(0,0,255);
 	Color WHITE(255,255,255);
 	
 	
