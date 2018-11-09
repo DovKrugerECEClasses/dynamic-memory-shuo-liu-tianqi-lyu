e9c0e7d60a72ace93c61165e37e05d2b35c005a8
leo81601 <36606875+leo81601@users.noreply.github.com>
2018-11-08 22:16:26 -0500

    Move all the methods definition in header file to String.cc file

--- dynamicmemory/String/String.cc
+++ dynamicmemory/String/String.cc
@@ -1,38 +1,61 @@
 #include "String.hh"
 
-String::String() {
-	
-}
+String::String():len(0), s(nullptr){}
 
-String::String(const char str[]) {
-
+String::String(const char str[]):len(strlen(str)), s(new char[len]) {
+	for (int i = 0; i < len; i++) {
+		s[i] = str[i];
+	}
 }
 	
 String::String(const char str[], uint32_t mult) {
-
+	uint32_t strsize = len / mult;
+	int c = 0;
+	for (int j = 0; j < mult; j++) {
+		for (int i = 0; i < strsize; i++, c++){
+			s[c] = str[i];
+		}
+	}
 }
 
 String::~String() {
-
+	delete [] s;
 }
 
 	//copy constructor
-String::String(const String& orig) {
-
+String::String(const String& orig):len(orig.len), s(new char[len]) {
+    uint64_t *src = (uint64_t *) orig.s;
+    uint64_t *des = (uint64_t *) s;
+    uint32_t count;
+    for (count = len; count >= 8; count -= 8) {
+        *des++ = *src++;
+    }
+    char* pdes = (char*)des;
+    char* psrc = (char*)src;
+    for (count = len; count > 0; count--) {
+        *pdes++ = *psrc++;
+    }
 }
-String& String::operator =(const String& orig) {
 
+String& String::operator =(const String& orig) {
+    String copy(orig);
+    len = orig.len;
+    swap(s, copy.s);
+    return *this;
 }
 
 char String::operator [](const uint32_t pos)const {
-
+    return s[pos];
 }
 char& String::operator [](uint32_t pos) {
-
+    return s[pos];
 }
 
 ostream& operator<<(ostream& s, const String& str) {
-
+    for (uint32_t i = 0; i < str.len; i++){
+        s << str[i];
+    }
+    return s;
 }
 
 	
--- dynamicmemory/String/String.hh
+++ dynamicmemory/String/String.hh
@@ -10,61 +10,21 @@ private:
 		return count;
 	}
 public:
-    String():len(0), s(nullptr){}
-    ~String() {
-        delete [] s;
-    }
+    String();
+    ~String();
 
-	String(const char str[]):len(strlen(str)), s(new char[len]) {
-        for (int i = 0; i < len; i++) {
-            s[i] = str[i];
-        }
-    }
+	String(const char str[]);
 
-	String(const char str[], uint32_t mult) {
-        uint32_t strsize = len / mult;
-        int c = 0;
-        for (int j = 0; j < mult; j++) {
-            for (int i = 0; i < strsize; i++, c++){
-                s[c] = str[i];
-            }
-        }
-    }
+	String(const char str[], uint32_t mult);
 
 	//copy constructor
-	String(const String& orig):len(orig.len), s(new char[len]) {
-        uint64_t *src = (uint64_t *) orig.s;
-        uint64_t *des = (uint64_t *) s;
-        uint32_t count;
-        for (count = len; count >= 8; count -= 8) {
-            *des++ = *src++;
-        }
-        char* pdes = (char*)des;
-        char* psrc = (char*)src;
-        for (count = len; count > 0; count--) {
-            *pdes++ = *psrc++;
-        }
-	}
+	String(const String& orig);
 
-	String& operator =(const String& orig) {
-        String copy(orig);
-        len = orig.len;
-        swap(s, copy.s);
-        return *this;
-    }
+	String& operator =(const String& orig);
 
-	char operator [](const uint32_t pos)const{
-        return s[pos];
-	}
+	char operator [](const uint32_t pos)const;
 
-	char& operator [](uint32_t pos){
-        return s[pos];
-	}
+	char& operator [](uint32_t pos);
 
-    friend ostream& operator<<(ostream& s, const String& str) {
-	    for (uint32_t i = 0; i < str.len; i++){
-	        s << str[i];
-	    }
-        return s;
-	}
+    friend ostream& operator<<(ostream& s, const String& str);
 };
