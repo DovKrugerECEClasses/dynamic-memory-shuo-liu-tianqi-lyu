141fde4240cfceee3dad6f40e8dc6758a3ec6170
leo81601 <36606875+leo81601@users.noreply.github.com>
2018-11-16 02:29:56 -0500

    Finish GrowArray header file

--- dynamicmemory/Templates/templatelist/GrowArray.hh
+++ dynamicmemory/Templates/templatelist/GrowArray.hh
@@ -3,49 +3,66 @@
 #include <iostream>
 
 template<typename T>
+
 class GrowArray {
 private:
     int used;
     int capacity;
     T* data;
     void checkGrow() {
-        if (  )
-            return; // don't grow unless you have to
+        if (used <= capacity){
+            return;
+        } else {
+            capacity = capacity * 2; // don't grow unless you have to
+        }
         T* old = data;
-
         //TODO: every time the list grows, double it!!!
         data = new T[used+1]; // calls T::T()
         for (int i = 0; i < used; i++)
             data[i] = old[i];
-        used++;
+        delete []old;
     }
 public:
     GrowArray() : used(0), capacity(0), data(nullptr) {}
-    GrowArray(int  initialCapacity)
-            : used(0), capacity(initialCapacity), data( ) //TODO: fix! {
-}
-
+    ~GrowArray() {
+        delete[]data;
+    }
+    GrowArray(int  initialCapacity):used(0), capacity(initialCapacity), data(new T[capacity]){} //TODO: fix!
 //TODO: add destructor, copy constructor, operator =
-void addEnd(const T& v) {
-    checkGrow();
-    data[length-1] = v;
-}
 
-T removeEnd() {
-    length--;
-    return data[length];
-}
-T operator [](int i) const {
-    return data[i];
-}
+    GrowArray(const GrowArray<T>& orig):used(orig.used), capacity(orig.capacity), data(new T[capacity]){
+        memcpy(data, orig.data, used);
+    }
 
-T& operator [](int i) {
-    return data[i];
-}
+    GrowArray& operator =(const GrowArray<T>& orig){
+        GrowArray copy(orig);
+        used = orig.used;
+        capacity = orig.capacity;
+        swap(data, copy.data);
+        return *this;
+    }
 
-friend std::ostream& operator <<(std::ostream& s, const GrowArray<T>& list) {
-    for (int i = 0; i < list.length; i++)
-        s << list[i] << ' ';
-    return s;
-}
+    void addEnd(const T& v) {
+        checkGrow();
+        data[used-1] = v;
+    }
+
+    T removeEnd() {
+        used--;
+        return data[used];
+    }
+
+    T operator [](int i) const {
+        return data[i];
+    }
+
+    T& operator [](int i) {
+        return data[i];
+    }
+
+    friend std::ostream& operator <<(std::ostream& s, const GrowArray<T>& list) {
+        for (int i = 0; i < list.used; i++)
+            s << list[i] << ' ';
+        return s;
+    }
 };
